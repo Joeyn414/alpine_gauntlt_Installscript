5c33298ffca290ff4744629f2248ec4cfbd4d7d9
Joe Niquette <joeyn414@gmail.com>
2016-08-10 14:18:48 -0400

    fixed the path errors

--- scripts/attacklocal.sh
+++ scripts/attacklocal.sh
@@ -16,16 +16,16 @@ cd $START_FOLDER
 git clone https://github.com/Joeyn414/alpine_gauntlt_Installscript.git
 
 #install gauntlt to the alpine container
-./opt/security/alpine_gauntlt_Installscript/scripts/gauntltTestInstall.sh
+./alpine_gauntlt_Installscript/scripts/gauntltTestInstall.sh
 
 echo "gauntlt install check initiating..."
-
-./opt/security/alpine_gauntlt_Installscript/scripts/ready_to_rumble.sh
+cd $START_FOLDER
+./alpine_gauntlt_Installscript/scripts/ready_to_rumble.sh
 echo "copying arachni attack alias file..."
 cp /opt/security/alpine_gauntlt_Installscript/scripts/gauntltattackaliases/arachni.json /usr/lib/ruby/gems/2.2.0/gems/gauntlt-1.0.12/lib/gauntlt/attack_aliases/arachni.json
 cd $START_FOLDER
 echo "attacking 127.0.0.2..."
-gauntlt /opt/security/alpine_gauntlt_Installscript/scripts/arachniattackfiles/arachni-all.attack | tee arachniout.txt
+gauntlt alpine_gauntlt_Installscript/scripts/arachniattackfiles/arachni-all.attack | tee arachniout.txt
 echo "checking the scan results, will exit 3 if we find vulnerabilities"
-./opt/security/alpine_gauntlt_Installscript/scripts/checkvulns.sh arachniout.txt
+./alpine_gauntlt_Installscript/scripts/checkvulns.sh arachniout.txt
 
