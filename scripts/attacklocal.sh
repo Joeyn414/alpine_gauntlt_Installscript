fb3c91689cc7191fc2fc509d12fa900cc7e1e475
Joe Niquette <joeyn414@gmail.com>
2016-08-10 13:14:40 -0400

    updated instructions inside attacklocal script

--- scripts/attacklocal.sh
+++ scripts/attacklocal.sh
@@ -1,4 +1,8 @@
-t clone pathtoscripts
+#!/bin/sh
+#This file will pull in the install files, install gauntlt to the container, and then run arachni against the local container. If it finds vulnerabilities the script will return exit 3 otherwise exit 0.
+apk update
+apk upgrade
+apk add git
 
 #install gauntlt to the alpine container
 ./opt/security/alpine_gauntlt_Installscript/scripts/gauntltTestInstall.sh
@@ -13,3 +17,4 @@ echo "attacking 127.0.0.2..."
 gauntlt /opt/security/alpine_gauntlt_Installscript/scripts/arachniattackfiles/arachni-all.attack | tee arachniout.txt
 echo "checking the scan results, will exit 3 if we find vulnerabilities"
 ./opt/security/alpine_gauntlt_Installscript/scripts/checkvulns.sh arachniout.txt
+
