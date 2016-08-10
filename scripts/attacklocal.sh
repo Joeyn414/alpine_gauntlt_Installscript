ceeb13b96e048f2e4caf7f1c4b014e8b30ed791d
Joe Niquette <joeyn414@gmail.com>
2016-08-10 13:02:05 -0400

    added file to run the install and attacks in one command

---
+++ scripts/attacklocal.sh
@@ -0,0 +1,15 @@
+t clone pathtoscripts
+
+#install gauntlt to the alpine container
+./opt/security/alpine_gauntlt_Installscript/scripts/gauntltTestInstall.sh
+
+echo "gauntlt install check initiating..."
+
+./opt/security/alpine_gauntlt_Installscript/scripts/ready_to_rumble.sh
+echo "copying arachni attack alias file..."
+cp /opt/security/alpine_gauntlt_Installscript/scripts/gauntltattackaliases/arachni.json /usr/lib/ruby/gems/2.2.0/gems/gauntlt-1.0.12/lib/gauntlt/attack_aliases/arachni.json
+cd $START_FOLDER
+echo "attacking 127.0.0.2..."
+gauntlt /opt/security/alpine_gauntlt_Installscript/scripts/arachniattackfiles/arachni-all.attack | tee arachniout.txt
+echo "checking the scan results, will exit 3 if we find vulnerabilities"
+./opt/security/alpine_gauntlt_Installscript/scripts/checkvulns.sh arachniout.txt
